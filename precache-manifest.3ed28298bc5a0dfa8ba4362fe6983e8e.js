a41c6e44f34fdc0e2fc0bd8d0dae3dc4f4ad44b4
Alvin Yang <alvin.yang73@gmail.com>
2019-03-19 16:44:35 -0700

    Updates

---
+++ precache-manifest.3ed28298bc5a0dfa8ba4362fe6983e8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1af0aebea77df3a062c",
+    "url": "/Calendar/static/js/runtime~main.81f9921a.js"
+  },
+  {
+    "revision": "61ed277b293eeceaaa24",
+    "url": "/Calendar/static/js/main.8524ded4.chunk.js"
+  },
+  {
+    "revision": "834e504d15304ee62a1a",
+    "url": "/Calendar/static/js/2.9d2d332d.chunk.js"
+  },
+  {
+    "revision": "61ed277b293eeceaaa24",
+    "url": "/Calendar/static/css/main.0ae26365.chunk.css"
+  },
+  {
+    "revision": "5c831a0e9ec280cae3101dfdb2fce1b1",
+    "url": "/Calendar/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/calendar/precache-manifest.12e552e479be583aace07d18b8d6e787.js"
+  "/Calendar/precache-manifest.3ed28298bc5a0dfa8ba4362fe6983e8e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/calendar/index.html", {
+workbox.routing.registerNavigationRoute("/Calendar/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
